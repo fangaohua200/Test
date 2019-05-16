39c5da92641c5049e37a0e64690b952f9ef78124
fangh1 <fangh1@lenovo.com>
2019-05-16 12:12:16 +0800

    test

--- src/javas/thread/catse1.java
+++ src/javas/thread/catse1.java
@@ -1,4 +1,4 @@
-package java.thread;
+package javas.thread;
 
 //  一、当两个并发线程访问同一个对象object中的这个synchronized(this)同步代码块时，一个时间内只能有一个线程得到执行。
 //另一个线程必须等待当前线程执行完这个代码块以后才能执行该代码块。
@@ -27,17 +27,17 @@ public class catse1 implements Runnable {
 
 
 	public static void main(String[] args) {
-//		catse1 t1 = new catse1(1000);
-//		Thread ta = new Thread(t1, "A");
-//		Thread tb = new Thread(t1, "B");
-//		ta.start();
-//		tb.start();
-		
-		catse1 t1 = new catse1(500);
-		catse1 t2 = new catse1(1000);
+		catse1 t1 = new catse1(1000);
 		Thread ta = new Thread(t1, "A");
-		Thread tb = new Thread(t2, "B");
+		Thread tb = new Thread(t1, "B");
 		ta.start();
 		tb.start();
+		
+//		catse1 t1 = new catse1(500);
+//		catse1 t2 = new catse1(1000);
+//		Thread ta = new Thread(t1, "A");
+//		Thread tb = new Thread(t2, "B");
+//		ta.start();
+//		tb.start();
 	}
 }
